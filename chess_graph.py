8e0c3b2b3b5fc19f1d2137f5499eeac4e5be794f
JasonShevik <jasonshevik@gmail.com>
2024-09-01 23:53:22 -0500

    Added more documentation to teh chess_graph.py file. Also implemented the get_pawn_edges() function.

--- chess_graph.py
+++ chess_graph.py
@@ -10,28 +10,41 @@ def create_blank_chess_graph() -> Tuple[torch.Tensor, torch.Tensor]:
     Used to create a graph of a chess board that shows where moves may be possible not including piece locations.
     :return: A tuple of edge_index and edge_type which are used to create a homogenous graph in PyTorch Geometric.
     """
+    # This list defines the connections that exist
     edges_list: List[List[int]] = []
+    # This list defines the type that each connection in edges_list is
     edge_types_list: List[int] = []
 
+    # This holds all of the different information for each piece type
+    # A function that returns a list for edges_list, and the corresponding value for edge_types_list
     pieces_list: List[Tuple[Callable[[], List[List[int]]], int]] = [
         (get_pawn_edges, 0),
         (get_knight_edges, 1),
-        (get_light_bishop_edges, 2),
-        (get_dark_bishop_edges, 3),
-        (get_rook_edges, 4),
-        (get_queen_edges, 5),
-        (get_king_edges, 6),
-        (get_en_passant_edges, 7)
+        (get_light_bishop_edges, 2),  # Dark and light bishops have same connection type.
+        (get_dark_bishop_edges, 2),  # No dark or light squares will be connected to each other, though.
+        (get_rook_edges, 3),
+        (get_queen_edges, 4),
+        (get_king_edges, 5),
+        (get_en_passant_edges, 6)
     ]
 
     def add_to_edges_list(the_list: List[List[int]], edge_function: Callable[[], List[List[int]]]) \
             -> Tuple[List[List[int]], int]:
-        count = 0
+        """
+        This function appends all of the edges in the current edge_function to the_list, and returns the number
+        of edges that there were.
+        :param the_list:
+        :param edge_function:
+        :return:
+        """
+        count: int = 0
         for edge in edge_function():
+            edge: List[int]
             the_list.append(edge)
             count += 1
         return the_list, count
 
+    #
     for this_edge_function, piece_type in pieces_list:
         edges_list, num = add_to_edges_list(edges_list, this_edge_function)
         edge_types_list.extend([piece_type] * num)
@@ -39,7 +52,7 @@ def create_blank_chess_graph() -> Tuple[torch.Tensor, torch.Tensor]:
     edge_index = torch.tensor(edges_list, dtype=torch.long).t().contiguous()
     edge_type = torch.tensor(edge_types_list, dtype=torch.long)
 
-    # The blank chess graph will not include castling because that depends on the square that the king is on.
+    # The blank chess graph will not include castling because that depends on the square that the king starts on.
     # A helper function get_castling_edges() should be called after adding piece placement to the graph.
 
     return edge_index, edge_type
@@ -50,9 +63,31 @@ def get_pawn_edges() -> List[List[int]]:
     Deterministically find all paths where pawns can move.
     :return: A list of edge pairs that show where pawn moves may be possible.
     """
+    #
+    def add_column(col_num: int) -> List[List[int]]:
+        pawn_column: List[List[int]] = []
+
+        # Can move 2 squares on first move
+        # From the front perspective
+        pawn_column.append([col_num + (8 * 1), col_num + (8 * 3)])
+        # From the back perspective
+        pawn_column.append([col_num + (8 * 6), col_num + (8 * 4)])
+
+        # All single square connections
+        # From the front perspective
+        for row_num in range(1, 7, 1):
+            pawn_column.append([col_num + (8 * row_num), col_num + (8 * (row_num + 1))])
+        # From the back perspective
+        for row_num in range(6, 0, -1):
+            pawn_column.append([col_num + (8 * row_num), col_num + (8 * (row_num - 1))])
+
+        return pawn_column
+
     edges: List[List[int]] = []
 
-    # Go through A through H for both sides. Include that you can move 1 or 2 on the first move.
+    # Go through A through H for both sides.
+    for column in range(0, 8, 1):
+        edges.extend(add_column(column))
 
     return edges
 
