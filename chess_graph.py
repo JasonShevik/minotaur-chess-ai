a5ee64f22a1c5a544b5f6b9a0a8052a6c4071f03
JasonShevik <jasonshevik@gmail.com>
2024-09-09 00:51:04 -0500

    Changed the edges_list in chess_graph.py to a set instead of a list to increase efficiency through faster lookup.

--- chess_graph.py
+++ chess_graph.py
@@ -11,14 +11,14 @@ def create_blank_chess_graph() -> Tuple[torch.Tensor, torch.Tensor]:
     :return: A tuple of edge_index and edge_type which are used to create a homogenous graph in PyTorch Geometric.
     """
     # This list defines the connections that exist
-    edges_list: List[Tuple[int, int]] = []
+    edges_list: set[Tuple[int, int]] = set()
     # This list defines the type that each connection in edges_list is
     edge_types_list: List[int] = []
 
     # This holds all of the different information for each piece type
     # A function that returns a list for edges_list, and the corresponding value for edge_types_list
     pieces_list: List[Tuple[Callable[[Tuple[int, int]],
-                                      List[Tuple[int, int]]],
+                                      set[Tuple[int, int]]],
                             Tuple[int, int],
                             int]] = [
         (get_pawn_edges,        (1, 0),  0),
@@ -35,15 +35,15 @@ def create_blank_chess_graph() -> Tuple[torch.Tensor, torch.Tensor]:
     for neighbor_function, start_square, piece_type in pieces_list:
         # Pawn edges are hard coded and don't use DFS
         if piece_type == 0:
-            new_edges: List[Tuple[int, int]] = get_pawn_edges()
+            new_edges: set[Tuple[int, int]] = get_pawn_edges()
         # En Passant edges are hard coded and don't use DFS
         elif piece_type == 6:
-            new_edges: List[Tuple[int, int]] = get_en_passant_edges()
+            new_edges: set[Tuple[int, int]] = get_en_passant_edges()
         else:
             # Get the list of new edges that are specific to this piece_type
-            new_edges: List[Tuple[int, int]] = depth_first_recursive(visited=[False for _ in range(64)],
+            new_edges: set[Tuple[int, int]] = depth_first_recursive(visited=[False for _ in range(64)],
                                                                      current_coordinates=start_square,
-                                                                     edges=[],
+                                                                     edges=set(),
                                                                      get_neighbors=neighbor_function)
 
         # Extend the edges_list to contain all of the newly gotten edges
@@ -63,35 +63,34 @@ def create_blank_chess_graph() -> Tuple[torch.Tensor, torch.Tensor]:
 # ##### ##### ##### ##### #####
 #   Piece connection getters
 
-def get_pawn_edges() -> List[Tuple[int, int]]:
+def get_pawn_edges() -> set[Tuple[int, int]]:
     """
     Deterministically find all paths where pawns can move.
     :return: A list of edges that show where all pawn moves may be possible.
     """
-    #
-    def add_column(col_num: int) -> List[Tuple[int, int]]:
+    def add_column(col_num: int) -> set[Tuple[int, int]]:
         # Can move 2 squares on first move
         # Front and back perspective
-        pawn_column: List[Tuple[int, int]] = [(col_num + (8 * 1), col_num + (8 * 3)),
-                                              (col_num + (8 * 6), col_num + (8 * 4))]
+        pawn_column: set[Tuple[int, int]] = {(col_num + (8 * 1), col_num + (8 * 3)),
+                                             (col_num + (8 * 6), col_num + (8 * 4))}
 
         # All single square moves
         for row_num in range(1, 7, 1): # From the front perspective
-            pawn_column.append((col_num + (8 * row_num), col_num + (8 * (row_num + 1))))
+            pawn_column.add((col_num + (8 * row_num), col_num + (8 * (row_num + 1))))
         for row_num in range(6, 0, -1): # From the back perspective
-            pawn_column.append((col_num + (8 * row_num), col_num + (8 * (row_num - 1))))
+            pawn_column.add((col_num + (8 * row_num), col_num + (8 * (row_num - 1))))
 
         return pawn_column
 
     # Go through A through H for both sides.
-    edges: List[Tuple[int, int]] = []
+    edges: set[Tuple[int, int]] = set()
     for column in range(8):
-        edges.extend(add_column(column))
+        edges.update(add_column(column))
 
     return edges
 
 
-def get_knight_neighbors(start_coordinates: Tuple[int, int]) -> List[Tuple[int, int]]:
+def get_knight_neighbors(start_coordinates: Tuple[int, int]) -> set[Tuple[int, int]]:
     """
     Gets the list of all possible knight moves from this position if the board were infinite.
     :param start_coordinates: The coordinates that the knight starts on in the format [row, column].
@@ -101,17 +100,17 @@ def get_knight_neighbors(start_coordinates: Tuple[int, int]) -> List[Tuple[int, 
     column: int
     row, column = start_coordinates
 
-    return remove_invalid_coordinates([(row + 2, column + 1), # Two up, one over
+    return remove_invalid_coordinates({(row + 2, column + 1), # Two up, one over
                                        (row + 2, column - 1),
                                        (row + 1, column + 2), # Two over, one up
                                        (row + 1, column - 2),
                                        (row - 1, column + 2), # Two over, one down
                                        (row - 1, column - 2),
                                        (row - 2, column + 1), # Two down, one over
-                                       (row - 2, column - 1)])
+                                       (row - 2, column - 1)})
 
 
-def get_bishop_neighbors(start_coordinates: Tuple[int, int]) -> List[Tuple[int, int]]:
+def get_bishop_neighbors(start_coordinates: Tuple[int, int]) -> set[Tuple[int, int]]:
     """
     Gets the list of all possible light bishop moves from this position.
     :param start_coordinates: The coordinates that the light bishop starts on in the format [row, column].
@@ -121,9 +120,9 @@ def get_bishop_neighbors(start_coordinates: Tuple[int, int]) -> List[Tuple[int, 
     column: int
     row, column = start_coordinates
 
-    neighbors: List[Tuple[int, int]] = []
+    neighbors: set[Tuple[int, int]] = set()
     for count in range(1, 8):
-        neighbors.extend([(row + count, column + count),  # NE Direction
+        neighbors.update([(row + count, column + count),  # NE Direction
                           (row - count, column + count),  # SE
                           (row - count, column - count),  # SW
                           (row + count, column - count)]) # NW
@@ -131,7 +130,7 @@ def get_bishop_neighbors(start_coordinates: Tuple[int, int]) -> List[Tuple[int, 
     return remove_invalid_coordinates(neighbors)
 
 
-def get_rook_neighbors(start_coordinates: Tuple[int, int]) -> List[Tuple[int, int]]:
+def get_rook_neighbors(start_coordinates: Tuple[int, int]) -> set[Tuple[int, int]]:
     """
     Gets the list of all possible rook moves from this position.
     :param start_coordinates: The coordinates that the rook starts on in the format [row, column].
@@ -141,9 +140,9 @@ def get_rook_neighbors(start_coordinates: Tuple[int, int]) -> List[Tuple[int, in
     column: int
     row, column = start_coordinates
 
-    neighbors: List[Tuple[int, int]] = []
+    neighbors: set[Tuple[int, int]] = set()
     for count in range(1, 8):
-        neighbors.extend([(row + count, column        ),  # N Direction
+        neighbors.update([(row + count, column        ),  # N Direction
                           (row,         column + count),  # E
                           (row - count, column        ),  # S
                           (row,         column - count)]) # W
@@ -151,7 +150,7 @@ def get_rook_neighbors(start_coordinates: Tuple[int, int]) -> List[Tuple[int, in
     return remove_invalid_coordinates(neighbors)
 
 
-def get_queen_neighbors(start_coordinates: Tuple[int, int]) -> List[Tuple[int, int]]:
+def get_queen_neighbors(start_coordinates: Tuple[int, int]) -> set[Tuple[int, int]]:
     """
     Gets the list of all possible queen moves from this position.
     :param start_coordinates: The coordinates that the queen starts on in the format [row, column].
@@ -160,7 +159,7 @@ def get_queen_neighbors(start_coordinates: Tuple[int, int]) -> List[Tuple[int, i
     return get_bishop_neighbors(start_coordinates) + get_rook_neighbors(start_coordinates)
 
 
-def get_king_neighbors(start_coordinates: Tuple[int, int]) -> List[Tuple[int, int]]:
+def get_king_neighbors(start_coordinates: Tuple[int, int]) -> set[Tuple[int, int]]:
     """
     Gets the list of all possible king moves from this position.
     :param start_coordinates: The coordinates that the king starts on in the format [row, column].
@@ -170,18 +169,18 @@ def get_king_neighbors(start_coordinates: Tuple[int, int]) -> List[Tuple[int, in
     column: int
     row, column = start_coordinates
 
-    return remove_invalid_coordinates([(row + row_offset, column + column_offset)
+    return remove_invalid_coordinates({(row + row_offset, column + column_offset)
                                         for row_offset in [-1, 0, 1]
                                         for column_offset in [-1, 0, 1]
-                                        if not (row_offset == 0 and column_offset == 0)])
+                                        if not (row_offset == 0 and column_offset == 0)})
 
 
-def get_en_passant_edges() -> List[Tuple[int, int]]:
+def get_en_passant_edges() -> set[Tuple[int, int]]:
     """
     Gets the list of all possible en passant moves.
     :return: A list of edges that show where all en passant moves may be possible.
     """
-    def get_diagonal_left_and_right(start: int) -> List[Tuple[int, int]]:
+    def get_diagonal_left_and_right(start: int) -> set[Tuple[int, int]]:
         """
         Gets the en passant edges for a specific square
         :param start: The index of the start square
@@ -194,24 +193,24 @@ def get_en_passant_edges() -> List[Tuple[int, int]]:
             spot: int = start - 8
 
         # Add all of the (correct) edges to the list
-        piece_edges: List[Tuple[int, int]] = []
-        for destination in [x for x in [spot + 1, spot - 1] if (x != 39 and x != 24)]:
-            piece_edges.append((start, destination))
+        piece_edges: set[Tuple[int, int]] = set()
+        for destination in [_ for _ in [spot + 1, spot - 1] if (x != 39 and x != 24)]:
+            piece_edges.add((start, destination))
 
         return piece_edges
 
-    edges: List[Tuple[int, int]] = []
+    edges: set[Tuple[int, int]] = set()
 
     # For every square on the fifth rank
     for square in range(32, 39, 1): # (your perspective)
-        edges.extend(get_diagonal_left_and_right(start=square))
+        edges.update(get_diagonal_left_and_right(start=square))
     for square in range(24, 31, 1): # (opponent's perspective)
-        edges.extend(get_diagonal_left_and_right(start=square))
+        edges.update(get_diagonal_left_and_right(start=square))
 
     return edges
 
 
-def get_castling_edges(board_vector: List[int]) -> List[Tuple[int, int]]:
+def get_castling_edges(board_vector: List[float]) -> set[Tuple[int, int]]:
     """
     This is a helper function to add castling edges to the graph. It needs to be done after the blank graph has
     been made and after the piece placement has been decided
@@ -219,12 +218,24 @@ def get_castling_edges(board_vector: List[int]) -> List[Tuple[int, int]]:
     castling and en passant.
     :return: A list of edge pairs that show where castling may be possible.
     """
-    edges: List[Tuple[int, int]] = []
+    edges: set[Tuple[int, int]] = set()
 
-    # Check if either side has castling rights. If so, we need to add edges. Otherwise, return the empty list.
-    if any(abs(x) > 6 for x in board_vector):
-        # Find the squares with the king and rooks
-        # Connect them with castling edges
+
+    for c in [1, -1]:
+        # Find the square with the king
+        for decimal in [0.1, 0.2, 0.3]:
+            num = board_vector.index((6 + decimal) * c)
+            if num :
+                # If it returns an index, connect that to the rooks
+
+                # Will this give me a list of indices?
+                rook = board_vector.index(4 * c)
+
+                # Connect king to the relevant side...
+                # If I need to check what decimal is to know what rook to connect to, then maybe I should remove the loop
+
+                # What if someone gets a pawn to the other side, chooses a rook, and moves it to their back rank?
+                # Can I distinguish between the original rook??
 
     return edges
 
@@ -241,20 +252,20 @@ def coordinates_to_index(coordinates: Tuple[int, int]) -> int:
     return (8 * coordinates[0]) + coordinates[1]
 
 
-def remove_invalid_coordinates(coordinate_list: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+def remove_invalid_coordinates(coordinate_list: set[Tuple[int, int]]) -> set[Tuple[int, int]]:
     """
     Removes the invalid moves (those not falling on the standard 8x8 board) from a list.
     :param coordinate_list: The list of theoretical moves that may go off the board.
     :return: A subset of the coordinate_list where all coordinates are within the bounds of a chess board.
     """
-    return [(row, column) for row, column in coordinate_list if all(0 <= coord <= 7 for coord in (row, column))]
+    return {(row, column) for row, column in coordinate_list if all(0 <= coord <= 7 for coord in (row, column))}
 
 
 def depth_first_recursive(visited: List[bool],
                           current_coordinates: Tuple[int, int],
-                          edges: List[Tuple[int, int]],
-                          get_neighbors: Callable[[Tuple[int, int]], List[Tuple[int, int]]]) \
-                          -> List[Tuple[int, int]]:
+                          edges: set[Tuple[int, int]],
+                          get_neighbors: Callable[[Tuple[int, int]], set[Tuple[int, int]]]) \
+                          -> set[Tuple[int, int]]:
     """
     The depth first graph traversal algorithm implemented recursively. Given a function to find the piece's neighbors.
     :param visited: A list representing the chess board that holds booleans for whether each square has been visited.
@@ -273,12 +284,12 @@ def depth_first_recursive(visited: List[bool],
         edge_from: Tuple[int, int] = (coordinates_to_index(destination), coordinates_to_index(current_coordinates))
         # If we haven't recorded these connections before, append them
         if edge_to not in edges:
-            edges.append(edge_to)
-            edges.append(edge_from)
+            edges.add(edge_to)
+            edges.add(edge_from)
 
             # If we haven't visited this destination yet, recursively call the function for that one
             if not visited[coordinates_to_index(destination)]:
-                edges.extend(depth_first_recursive(visited=visited,
+                edges.update(depth_first_recursive(visited=visited,
                                                    current_coordinates=destination,
                                                    edges=edges,
                                                    get_neighbors=get_neighbors))
